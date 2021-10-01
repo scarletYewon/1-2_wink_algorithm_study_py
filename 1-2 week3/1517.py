c12757fb1f4271fa5cacbc61653fcc38bc9bf45b
scarletYewon <kwn09032@gmail.com>
2021-10-01 18:39:15 +0900

    Update 1517.py

--- 1-2 week3/1517.py
+++ 1-2 week3/1517.py
@@ -1,36 +1,39 @@
 import sys
-input=sys.stdin.readline
 
-def merge(a,b):
-    global cnt
-    la,lb=len(a),len(b)
-    i,j=0,0
-    temp=[]
-    while i<la and j<lb:
-        if a[i]>b[j]:
-            temp.append(b[j])
-            j+=1
-            cnt+=la-i
-        else:
-            temp.append(a[i])
-            i+=1
-    if i==la:
-        temp.extend(b[j:])
-    else:
-        temp.extend(a[i:])
-    return temp
+read = lambda: sys.stdin.readline().rstrip()
 
-def merge_sort(arr):
-    if len(arr)<=1:
-        return arr
-    left=0
-    right=len(arr)-1
-    mid=(left+right)//2
-    return merge(merge_sort(arr[left:mid+1]),merge_sort(arr[mid+1:]))
 
-n=int(input())
-cnt=0
-arr=list(map(int,input().split()))
-merge_sort(arr)
-print(cnt)
+def merge_sort(start, end):
+    global swap_count, A
 
+    if start < end:
+        mid = (start + end) // 2
+        merge_sort(start, mid)
+        merge_sort(mid + 1, end)
+
+        a, b = start, mid + 1
+        temp = []
+
+        while a <= mid and b <= end:
+            if A[a] <= A[b]:
+                temp.append(A[a])
+                a += 1
+            else:
+                temp.append(A[b])
+                b += 1
+                swap_count += (mid - a + 1)
+
+        if a <= mid:
+            temp = temp + A[a:mid + 1]
+        if b <= end:
+            temp = temp + A[b:end + 1]
+
+        for i in range(len(temp)):
+            A[start + i] = temp[i]
+
+
+swap_count = 0
+N = int(read())
+A = list(map(int, read().split()))
+merge_sort(0, N - 1)
+print(swap_count)
