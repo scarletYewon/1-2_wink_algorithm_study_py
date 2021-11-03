a21f3761f5a040adf1e99f72795362f7aeb47ad5
이예원 <81704418+scarletYewon@users.noreply.github.com>
2021-11-03 20:56:44 +0900

    Create 9251.py

---
+++ 1-2 week4/9251.py
@@ -0,0 +1,15 @@
+import sys
+
+A = sys.stdin.readline().strip().upper()
+B = sys.stdin.readline().strip().upper()
+
+lcs = [[0] * (len(A)+1) for _ in range(len(B)+1)]
+
+for i in range(1,len(B)+1) :
+  for j in range(1,len(A)+1) :
+    if B[i-1] == A[j-1] :
+        lcs[i][j] = lcs[i-1][j-1] + 1
+    else :
+        lcs[i][j] = max(lcs[i][j-1], lcs[i-1][j])
+
+print(lcs[-1][-1])
